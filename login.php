de130e924c645d8687d1a018e2da28835b355758
michalani <27767884+michalani@users.noreply.github.com>
2019-10-29 22:32:27 +0000

    Update login.php

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
     <?php
-        if ($_POST['password'] == '{Perry_very_berry}') {
+        if ($_POST['pass'] == '{Perry_very_berry}') {
         echo 'http://cybersoc.cf';
         } else {
         echo 'Hmm we couldn't find what you were looking for, try again?';
