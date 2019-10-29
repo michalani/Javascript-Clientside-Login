571b2189a05a47be3b10e38bafa77719a92237f6
michalani <27767884+michalani@users.noreply.github.com>
2019-10-29 22:38:22 +0000

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,12 @@
     <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $name = test_input($_POST["name"]);
         if ($_POST['pass'] == '{Perry_very_berry}') {
         echo 'http://cybersoc.cf';
-        } else {
+        } else if($pass == 'perry'){
+         echo 'perry is right';   
+        }
+        else {
         echo 'Hmm we couldn't find what you were looking for, try again?';
         }
     ?>
