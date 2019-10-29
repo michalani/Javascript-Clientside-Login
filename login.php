acac8fc9e6de6db41656d254bb1ebb0d95a213c2
michalani <27767884+michalani@users.noreply.github.com>
2019-10-29 22:11:58 +0000

    Create login.php

---
+++ login.php
@@ -0,0 +1,7 @@
+    <?php
+        if ($_POST['password'] == '{Perry_very_berry}') {
+        echo 'http://cybersoc.cf';
+        } else {
+        echo 'Hmm we couldn't find what you were looking for, try again?';
+        }
+    ?>
